cb308f32e74682cd0dd95511fe50529bca1f23ee
VIKTORCORADOV <viktorcoradov@outlook.com>
2014-09-22 23:30:25 -0600

    comment

--- cargarCaracteristicas.php
+++ cargarCaracteristicas.php
@@ -22,6 +22,7 @@ while ($row = mysql_fetch_array($result, MYSQL_ASSOC)) {
     echo "<td style=\"text-align:center\">$descripcion</td>";
     echo "<td style=\"text-align:center\">$salon</td>";
 	// Comentario hecho por Mario //
+	// Comentario hecho por victor //
     echo "</tr>";
 }
 
